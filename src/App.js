77bdfdcbec8c8e0c6ae9dd5f5ccec2291c96c2c1
prasetyoww <rooms.wibowo3@gmail.com>
2022-08-23 08:15:16 +0700

    upgrade

--- src/App.js
+++ src/App.js
@@ -56,8 +56,6 @@ function App() {
         {githubUsername}
         </Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
         </Card.Text>
         <Button variant="primary" onClick = {repoDataUrl} >List My Public Repos !</Button>
       </Card.Body>
